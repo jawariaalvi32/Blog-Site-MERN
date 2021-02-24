8725170e38b13f9f4a884a3ae95c89169cb83661
jawariaalvi32 <jawariaalvi32@gmail.com>
2021-02-25 00:49:43 +0500

    Header

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,19 +1,16 @@
-import { Avatar } from "@material-ui/core";
+import { Avatar, Button } from "@material-ui/core";
 import React, { useState } from "react";
-// import { useDispatch, useSelector } from "react-redux";
-// import {
-//   selectSignedIn,
-//   selectUserData,
-//   setInput,
-//   setSignedIn,
-//   setUserData,
-// } from "../features/userSlice";
-
+import { useDispatch, useSelector } from "react-redux";
+import DialogBox from "../DialogBox";
 import "./Header.css";
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   const [inputValue, setInputValue] = useState("tech");
-//   const isSignedIn = useSelector(selectSignedIn);
+  const user = useSelector( state => state.user );
+  const isSignedIn = (user.name) ? true : false;
+  console.log("DDD",user)
+  console.log(isSignedIn)
 //   const userData = useSelector(selectUserData);
 
 //   const dispatch = useDispatch();
@@ -30,46 +27,33 @@ const Navbar = () => {
 
   return (
     <div className="navbar">
-      <h1 className="navbar__header">BlogMania 💬</h1>
-      {/* {isSignedIn && (
-        <div className="blog__search">
-          <input
-            className="search"
-            placeholder="Search for a blog"
-            value={inputValue}
-            onChange={(e) => setInputValue(e.target.value)}
-          />
-          <button className="submit" onClick={handleClick}>
-            Search
-          </button>
+      <Link to="/"><h1 className="navbar__header">BlogMania 💬</h1></Link>
+      {isSignedIn && (
+        <div>
+          <Link to="/addpost">Create Blog</Link>
+          <Link to="/posts">View Blog</Link>
         </div>
-      )} */}
+      )}
 
-      {/* {isSignedIn ? (
+      {isSignedIn ? (
         <div className="navbar__user__data">
-          <Avatar
+          {/* <Avatar
             className="user"
             src={userData?.imageUrl}
             alt={userData?.name}
-          />
-          <h1 className="signedIn">{userData?.givenName}</h1>
-          <GoogleLogout
-            clientId="57529085775-fk8rn8hren1q8o5ja2idq4m7hug5aong.apps.googleusercontent.com"
-            render={(renderProps) => (
-              <button
-                onClick={renderProps.onClick}
-                disabled={renderProps.disabled}
-                className="logout__button"
-              >
-                Logout 😦
-              </button>
-            )}
-            onLogoutSuccess={logout}
-          />
+          /> */}
+          <h1 className="signedIn">{user.name}</h1>
+          <button
+            // onClick={renderProps.onClick}
+            className="logout__button"
+          >
+            Logout 😦
+          </button>
         </div>
       ) : (
-        <h1 className="notSignedIn">User not available 😞</h1>
-      )} */}
+        <Link to="#"><DialogBox/></Link>
+        // <h1 className="notSignedIn">User not available 😞</h1>
+      )}
     </div>
   );
 };
