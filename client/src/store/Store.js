b353b4e1bc2a5e3b8473472490ff6d909f50e8e5
jawariaalvi32 <jawariaalvi32@gmail.com>
2021-02-25 00:52:03 +0500

    redux

---
+++ client/src/store/Store.js
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit';
+import userReducer from './slices/UserSlice';
+
+
+export default  configureStore({
+  reducer: {
+    user: userReducer,
+    // todo: todoReducer,
+    // post: postReducer
+  },
+});
