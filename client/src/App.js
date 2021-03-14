f4eedff923b74d6efca061e0624ffff8adb921e3
fazzyA <faz.pak@gmail.com>
2021-03-14 23:02:12 +0500

    remove

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,21 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-
-import Add from "./components/Add";
-import Users from "./components/Users";
-// import Header from "./components/Header";
 import Header from "./components/header/Header";
-import SingleUser from "./components/SingleUser";
-import AddPost from "./components/AddPost";
-import Posts from "./components/Posts";
-// import SinglePost from "./components/SinglePost";
 import SingleBlog from "./components/blog/SingleBlog";
 import BlogCard from "./components/blog/Blog";
 import CreateBlog from "./components/blog/CreateBlog";
+import SignUp from "./components/user/SignUp";
+
+
 function App() {
   return (
     <Router>
       <div>
         <Header />
-        {/* <CreateBlog/> */}
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
         <Switch>
-          <Route path="/register">
-            <Add />
-          </Route>
-          <Route path="/addpost">
-            <AddPost />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
-          {/* <Route path="/posts">
-            <Posts />
-          </Route> */}
-          <Route path="/single-user/:id">
-            <SingleUser />
+          <Route path="/SignUp">
+            <SignUp />
           </Route>
           <Route path="/posts/:id">
             <SingleBlog />
